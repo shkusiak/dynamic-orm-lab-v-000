6b8fce23e7fc8bfe77712aecec10d4b05b03b0e4
Shea Kusiak <shkusiak@gmail.com>
2018-06-29 20:58:21 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,4 +53,9 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
+  def self.find_by(attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute} = ''"
+    DB[:conn].execute(sql)
+  end
+
 end
