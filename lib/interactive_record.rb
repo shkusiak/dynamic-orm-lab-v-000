b3a23b61e7f5d77203768f8d1389951c90482ac7
Shea Kusiak <shkusiak@gmail.com>
2018-06-29 20:53:20 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
@@ -13,6 +14,27 @@ class InteractiveRecord
   end
 
   def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
   end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
 
 end
